ca43817b4e59bde4bcf5193969a9241734bcb420
Chrisini <c_b@live.at>
2024-01-22 18:27:43 +0800

    featuremaps are working now

--- helper/visualisation/hook.py
+++ helper/visualisation/hook.py
@@ -8,8 +8,10 @@ class Hook():
 
     def __init__(self, module):
         self.hook = module.register_forward_hook(self.hook_fn)
+        
     def hook_fn(self, module, input, output):
         self.input = input
         self.output = output
+        
     def close(self):
         self.hook.remove()
